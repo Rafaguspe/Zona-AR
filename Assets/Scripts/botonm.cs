a204e2170cdb247211a4b595fef2aca13fb8d5e4
JosueJB88 <josuearismendy04@gmail.com>
2022-06-24 22:48:23 -0400

    hhhhh

---
+++ Assets/Scripts/botonm.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Botonesdelscroll : MonoBehaviour
+{
+	public Button flechaDerecha, flechaizquierda;
+	public GameObject Contenedor;
+
+	
+	void star(){
+		
+		flechaDerecha.onClick.AddListener(()=> moverD());
+	}
+	
+	void moverD(){
+		
+		Contenedor.transform.position = new	Vector3(-1761,-7,0);
+		
+		
+	}
+	
+}
